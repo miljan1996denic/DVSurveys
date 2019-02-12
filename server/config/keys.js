c36da6d861b2026e6be1c10030aa505e75645e1a
miljandenic96 <miljan1996denic@gmail.com>
2019-02-12 18:58:27 +0100

    update

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,1 +1,10 @@
-module.exports = require('./dev');
+module.exports = {
+    GoogleClientID: '15282274421-en3q7vqpffs7i31i3juk3tip33jpnkug.apps.googleusercontent.com',
+    GoogleClientSecret: 'f_N1Q8MrXNhXfO5EZSZJbrla',
+    mongoURI: 'mongodb://miljan.denic:gorskivjenac1@ds121295.mlab.com:21295/emaily-dev-dvs',
+    cookieKey: 'fasgfagaegfafasfabbganfuainbcianbaiauhuaifnhauifiafawuiobna',
+    stripePublishableKey: 'pk_test_cVf4ni0Q77SoP3LhcS07U0LG',
+    stripeSecretKey: 'sk_test_MYRkarwACXD7wU1UfZi2EY7W',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+}
